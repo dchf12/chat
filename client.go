ea61edd179816e2679f81848a0d4c39a83cc12a5
dchf <dchf.jp@gmail.com>
2023-05-09 18:43:20 +0900

    websocketへの読み書き処理作成

--- client.go
+++ client.go
@@ -7,3 +7,24 @@ type client struct {
 	send   chan []byte
 	room   *room
 }
+
+func (c *client) read() {
+	for {
+		var msg []byte
+		if err := websocket.Message.Receive(c.socket, &msg); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := websocket.Message.Send(c.socket, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
