30f302844a067d1bf1bec8541b14be8633eeb51a
dchf <dchf.jp@gmail.com>
2023-05-14 15:46:41 +0900

    記録無効化処理の実装

--- main.go
+++ main.go
@@ -5,11 +5,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 	"sync"
-
-	"github.com/dchf12/chat/trace"
 )
 
 type templateHandler struct {
@@ -31,7 +28,6 @@ func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
 	r := newRoom()
-	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
--- trace/tracer.go
+++ trace/tracer.go
@@ -18,6 +18,14 @@ func (t *tracer) Trace(a ...any) {
 	t.out.Write([]byte("\n"))
 }
 
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(a ...any) {}
+
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
+}
+
+func Off() Tracer {
+	return &nilTracer{}
 }
