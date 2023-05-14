f772150b7b03038b0b2b87c6a0e3824b8df1c4fc
dchf <dchf.jp@gmail.com>
2023-05-14 15:27:22 +0900

    tracer構造体とNew関数実装

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,5 +1,23 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 type Tracer interface {
 	Trace(...any)
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...any) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
 }
